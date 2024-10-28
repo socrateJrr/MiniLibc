64621223418705f9cf5fcb2abeb9bfa05d0e8f53
socrateJrr <@delia.voicu>
2024-10-28 23:23:14 +0200

    nu mai pot

--- src/mm/malloc.c
+++ src/mm/malloc.c
@@ -6,32 +6,68 @@
 #include <sys/mman.h>
 #include <string.h>
 #include <stdlib.h>
+#include <errno.h>
+
+extern struct mem_list mem_list_head = {NULL, 0, NULL, NULL};
 
 void *malloc(size_t size)
 {
-	/* TODO: Implement malloc(). */
-	return NULL;
+	if (size == 0)
+	{
+		errno = EINVAL;
+		return NULL;
+	}
+	struct mem_list *aux;
+	while (aux)
+	{
+		if (aux->len >= size)
+		{
+			aux->len = aux->len - size;
+			void *ptr = aux->start;
+			aux->start = NULL;
+			return ptr;
+		}
+		aux = aux->next;
+	}
+	void *new = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (new == MAP_FAILED)
+	{
+		errno = ENOMEM;
+		return NULL;
+	}
+	if (mem_list_add(new, size) == -1)
+	{
+		munmap(new, size);
+		errno = ENOMEM;
+		return NULL;
+	}
+	return new;
 }
-
 void *calloc(size_t nmemb, size_t size)
 {
-	/* TODO: Implement calloc(). */
+
 	return NULL;
 }
-
 void free(void *ptr)
 {
-	/* TODO: Implement free(). */
+	if (ptr == NULL)
+		return;
+	struct mem_list *new = mem_list_find(ptr);
+	if (new)
+	{
+		munmap(ptr, new->len);
+		mem_list_del(ptr);
+	}
 }
 
 void *realloc(void *ptr, size_t size)
 {
-	/* TODO: Implement realloc(). */
+
 	return NULL;
 }
 
 void *reallocarray(void *ptr, size_t nmemb, size_t size)
 {
-	/* TODO: Implement reallocarray(). */
+
 	return NULL;
 }
--- src/mm/mmap.c
+++ src/mm/mmap.c
@@ -1,23 +1,41 @@
 // SPDX-License-Identifier: BSD-3-Clause
-
+#include <internal/mm/mem_list.h>
+#include <internal/types.h>
 #include <sys/mman.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <errno.h>
-#include <internal/syscall.h>
+#include <string.h>
 
 void *mmap(void *addr, size_t length, int prot, int flags, int fd, off_t offset)
 {
-	/* TODO: Implement mmap(). */
-	return MAP_FAILED;
+	long int apel = syscall(9, addr, length, prot, flags, fd, offset);
+	if (apel < 0)
+	{
+		errno = -apel;
+		return MAP_FAILED;
+	}
+	return (void *)apel;
 }
 
 void *mremap(void *old_address, size_t old_size, size_t new_size, int flags)
 {
-	/* TODO: Implement mremap(). */
-	return MAP_FAILED;
+	long int new = syscall(25, old_address, old_size, new_size, flags);
+	if (new < 0)
+	{
+		errno = -new;
+		return MAP_FAILED;
+	}
+	return (void *)new;
 }
 
 int munmap(void *addr, size_t length)
 {
-	/* TODO: Implement munmap(). */
-	return -1;
+	long int apel = syscall(11, addr, length);
+	if (apel < 0)
+	{
+		errno = -apel;
+		return -1;
+	}
+	return 0;
 }
